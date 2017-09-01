09085e3b2e6ad94901daf090d4083510a32ee3b2
rodiontopagent <rodion.tokovchuk@gmail.com>
2017-09-01 10:25:19 +0300

    Queue added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,10 @@
 import {_l} from '../lib/_utl.js';
 import {LinkedList} from '../lib/data-structure/linked-list/Linked_List.js';
 import {Stack} from '../lib/data-structure/stack/Stack.js';
+import {Queue} from '../lib/data-structure/queue/Queue.js';
 
-var stack = new Stack();
+var queue = new Queue(),
+    data = [1,2,3,4,5];
+queue.store(1);
+_l(queue.isEmpty());
 
-_l(stack.isEmpty());
-
-var data = [1,2,3,4,5,6,7];
-
-data.map(el=>stack.push(el));
-
-_l(stack.pop());
-_l(stack.length());
-
-stack.clean(true);
-_l(stack);
