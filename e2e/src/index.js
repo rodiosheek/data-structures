7b269506f4298b628bbe656192292c0fd978d375
rodiontopagent <rodion.tokovchuk@gmail.com>
2017-09-04 18:33:12 +0300

    Tests uncomment

--- e2e/src/index.js
+++ e2e/src/index.js
@@ -3,30 +3,30 @@ import {StackSpec} from './StackSpec.js';
 import {QueueSpec} from './QueueSpec.js';
 import {SetSpec} from './SetSpec.js';
 
-// LinkedListSpec.run('init');
-// LinkedListSpec.run('append');
-// LinkedListSpec.run('prepend');
-// LinkedListSpec.run('deleteByVal');
-// LinkedListSpec.run('contains');
-// LinkedListSpec.run('clean');
-// LinkedListSpec.run('length');
-// LinkedListSpec.run('lastItem');
-// LinkedListSpec.run('toArray');
-//
-// StackSpec.run('init');
-// StackSpec.run('push');
-// StackSpec.run('pop');
-// StackSpec.run('pip');
-// StackSpec.run('length');
-// StackSpec.run('isEmpty');
-// StackSpec.run('clean');
-//
-// QueueSpec.run('init');
-// QueueSpec.run('store');
-// QueueSpec.run('access');
-// QueueSpec.run('peek');
-// QueueSpec.run('isEmpty');
-// QueueSpec.run('length');
+LinkedListSpec.run('init');
+LinkedListSpec.run('append');
+LinkedListSpec.run('prepend');
+LinkedListSpec.run('deleteByVal');
+LinkedListSpec.run('contains');
+LinkedListSpec.run('clean');
+LinkedListSpec.run('length');
+LinkedListSpec.run('lastItem');
+LinkedListSpec.run('toArray');
+
+StackSpec.run('init');
+StackSpec.run('push');
+StackSpec.run('pop');
+StackSpec.run('pip');
+StackSpec.run('length');
+StackSpec.run('isEmpty');
+StackSpec.run('clean');
+
+QueueSpec.run('init');
+QueueSpec.run('store');
+QueueSpec.run('access');
+QueueSpec.run('peek');
+QueueSpec.run('isEmpty');
+QueueSpec.run('length');
 
 SetSpec.run('init');
 SetSpec.run('add');
